e4365480eb17d964da3be13a7776fd2fe481f751
tyatoshi <>
2020-03-17 04:32:06 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,10 @@ class ApplicationController < ActionController::Base
     @count_microposts = user.microposts.count
   end
   
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+  end
+  
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,8 @@
 class ToppagesController < ApplicationController
   def index
-    @micropost = current_user.microposts.build
-    @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+    end
   end
 end
